960cb43751b6d2092eaa6ba07e1b9a34dbdd58e4
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-05 15:36:42 -0300

    feat: Cria classe ImpostoController e metodo listarTodosImpostos com o endpoint GetMapping

--- src/main/java/br/com/zup/impostos/controllers/ImpostoController.java
+++ src/main/java/br/com/zup/impostos/controllers/ImpostoController.java
@@ -1,4 +1,27 @@
 package br.com.zup.impostos.controllers;
 
+import br.com.zup.impostos.models.Imposto;
+import br.com.zup.impostos.services.ImpostoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tipos")
 public class ImpostoController {
+    private final ImpostoService impostoService;
+
+    public ImpostoController(ImpostoService impostoService) {
+        this.impostoService = impostoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Imposto>> listarTodosImpostos(){
+        List<Imposto> impostos = impostoService.todosImpostos();
+        return new ResponseEntity<>(impostos, HttpStatus.OK);
+    }
 }
