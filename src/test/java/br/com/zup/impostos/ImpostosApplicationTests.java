06bd1edd6b5674630c06e836e625ecdfa4eec586
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-05 08:45:20 -0300

    projeto

---
+++ src/test/java/br/com/zup/impostos/ImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.impostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
