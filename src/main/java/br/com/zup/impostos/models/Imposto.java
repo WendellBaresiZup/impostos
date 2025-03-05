c9362f8d4c1f433e963fc7cb8dbcd11f705b69c9
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-05 14:23:35 -0300

    feat: Adiciona pacote models

---
+++ src/main/java/br/com/zup/impostos/models/Imposto.java
@@ -0,0 +1,56 @@
+package br.com.zup.impostos.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Imposto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private double aliquota;
+
+    public Imposto(Long id, String nome, String descricao, double aliquota) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.aliquota = aliquota;
+    }
+
+    public Imposto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(double aliquota) {
+        this.aliquota = aliquota;
+    }
+}
