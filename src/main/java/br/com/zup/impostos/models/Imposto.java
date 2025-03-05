f9e950bcfa07a4cf28c18cd710b5703feec3e3a9
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-05 17:14:59 -0300

    refactor: Implementado anotation de entitade na classe Imposto

--- src/main/java/br/com/zup/impostos/models/Imposto.java
+++ src/main/java/br/com/zup/impostos/models/Imposto.java
@@ -1,9 +1,11 @@
 package br.com.zup.impostos.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Imposto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
