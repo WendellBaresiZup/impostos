7cd8c3dca0898b73c70e56c1f853b5217b91fa88
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-05 14:54:11 -0300

    feat: Cria classe ImpostoService e o metodo calcularImposto

--- src/main/java/br/com/zup/impostos/services/ImpostoService.java
+++ src/main/java/br/com/zup/impostos/services/ImpostoService.java
@@ -1,4 +1,13 @@
 package br.com.zup.impostos.services;
 
+import br.com.zup.impostos.models.Imposto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ImpostoService {
+
+
+    public Double calcularImposto(Imposto imposto, Double valorBase){
+        return valorBase * imposto.getAliquota() / 100;
+    }
 }
