670a2e22ff1bd3f538ba23d1d6faa6ec406fcc90
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-06 09:00:39 -0300

    feat: Cria metodo para gerar token, com tempo inicial e tempo de expiracao

--- src/main/java/br/com/zup/impostos/infra/JwtUtil.java
+++ src/main/java/br/com/zup/impostos/infra/JwtUtil.java
@@ -1,5 +1,21 @@
 package br.com.zup.impostos.infra;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 public class JwtUtil {
     private String SECRET_KEY = "secreta";
+
+    public String geradorToken(String userName){
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+
 }
