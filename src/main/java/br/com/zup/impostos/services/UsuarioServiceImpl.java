a84a0e97049aa38ef37b788981db2aa12b9e2f61
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-06 10:47:20 -0300

    feat: Cria metodo para fazer login na logica de servico da aplicacao

--- src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.zup.impostos.services;
 
+import br.com.zup.impostos.infra.JwtUtil;
+import br.com.zup.impostos.models.LoginRequest;
 import br.com.zup.impostos.models.Usuario;
 import br.com.zup.impostos.repositories.UsuarioRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,9 +10,13 @@ public class UsuarioServiceImpl implements UsuarioService{
 
     private final UsuarioRepository usuarioRepository;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+    private final JwtUtil jwtUtil;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, JwtUtil jwtUtil) {
         this.usuarioRepository = usuarioRepository;
+        this.jwtUtil = jwtUtil;
     }
+
 
     @Override
     public Usuario cadastrarUsuario(Usuario usuario){
@@ -19,5 +25,15 @@ public class UsuarioServiceImpl implements UsuarioService{
         }
         usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
         return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public String login(LoginRequest loginRequest){
+        Usuario usuario = (Usuario) usuarioRepository.findByUserName(loginRequest.getUserName())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!") );
+        if (!new BCryptPasswordEncoder().matches(loginRequest.getPassword(), usuario.getPassword())){
+            throw new RuntimeException("Senha inválida!");
+        }
+        return jwtUtil.geradorToken(usuario.getUserName());
     }
 }
