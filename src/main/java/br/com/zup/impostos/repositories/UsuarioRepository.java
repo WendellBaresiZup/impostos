5437310b5c3e8951d805a283e4c94c2f2352bda1
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-06 10:05:10 -0300

    feat: Cria metodo BuscarPeloNome para gerar query que vai buscar pelo nome do usuario dentro do banco de dados

--- src/main/java/br/com/zup/impostos/repositories/UsuarioRepository.java
+++ src/main/java/br/com/zup/impostos/repositories/UsuarioRepository.java
@@ -3,5 +3,8 @@ package br.com.zup.impostos.repositories;
 import br.com.zup.impostos.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Object> findByUserName(String userName);
 }
