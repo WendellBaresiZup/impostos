140aaed7e8e9bcfd1d64793755d8d4964fe45bd6
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-06 10:02:27 -0300

    feat: Cria metodo criarUsuario na logica de servicos da aplicacao

--- src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
@@ -1,6 +1,8 @@
 package br.com.zup.impostos.services;
 
+import br.com.zup.impostos.models.Usuario;
 import br.com.zup.impostos.repositories.UsuarioRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UsuarioServiceImpl implements UsuarioService{
 
@@ -10,4 +12,12 @@ public class UsuarioServiceImpl implements UsuarioService{
         this.usuarioRepository = usuarioRepository;
     }
 
+    @Override
+    public Usuario criarUsuario(Usuario usuario){
+        if (usuarioRepository.findByUserName(usuario.getUserName()).isPresent()){
+            throw new RuntimeException("Username já existe!");
+        }
+        usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
+        return usuarioRepository.save(usuario);
+    }
 }
