a1833eb0a65f0be0fa47bc8810306c99fbcd7122
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-06 14:16:39 -0300

    refactor: Alterado o atributo SECRET_KEY que agora vai receber uma chave de 256bits

--- src/main/java/br/com/zup/impostos/infra/JwtUtil.java
+++ src/main/java/br/com/zup/impostos/infra/JwtUtil.java
@@ -4,19 +4,23 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
+
 @Component
 public class JwtUtil {
-    private String SECRET_KEY = "secreta";
+    private Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
 
     public String geradorToken(String userName){
         return Jwts.builder()
                 .setSubject(userName)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
