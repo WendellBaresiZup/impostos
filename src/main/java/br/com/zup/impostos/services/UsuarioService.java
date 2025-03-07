c03cab0ef8094230026feca531f3b39573efbc4c
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-07 09:31:19 -0300

    refactor: Implementado os parametros do UsuarioLoginDTO dentro da interface UsuarioService

--- src/main/java/br/com/zup/impostos/services/UsuarioService.java
+++ src/main/java/br/com/zup/impostos/services/UsuarioService.java
@@ -1,9 +1,9 @@
 package br.com.zup.impostos.services;
 
-import br.com.zup.impostos.models.LoginRequest;
+import br.com.zup.impostos.dto.UsuarioLoginDTO;
 import br.com.zup.impostos.models.Usuario;
 
 public interface UsuarioService {
     Usuario cadastrarUsuario(Usuario usuario);
-    String login(LoginRequest loginRequest);
+    String login(UsuarioLoginDTO usuarioLoginDTO);
 }
