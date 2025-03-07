961cd2eb750b65c0327fa052ba61afba3c4a0d8b
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-07 09:56:56 -0300

    feat: Cria a classe de excecao de usuario nao encontrado

--- src/main/java/br/com/zup/impostos/exceptions/UsuarioNotFoundException.java
+++ src/main/java/br/com/zup/impostos/exceptions/UsuarioNotFoundException.java
@@ -1,4 +1,7 @@
 package br.com.zup.impostos.exceptions;
 
-public class UsuarioNotFoundException {
+public class UsuarioNotFoundException extends RuntimeException{
+    public UsuarioNotFoundException(String message){
+        super(message);
+    }
 }
