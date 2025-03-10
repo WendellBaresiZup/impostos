d6195c451105eeb1be0bc2753812257aaac8d7f3
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-10 15:12:46 -0300

    refactor: Retira atributo role, e getter e setter. Inseri anotation do lombook

--- src/main/java/br/com/zup/impostos/models/Usuario.java
+++ src/main/java/br/com/zup/impostos/models/Usuario.java
@@ -2,56 +2,27 @@ package br.com.zup.impostos.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
+@Data
 public class Usuario {
     @Id
     @UuidGenerator
     private String uuid;
+    @NotBlank(message = "Nome é obrigatório")
     private String userName;
+    @NotBlank(message = "Senha é obrigatória")
     private String password;
-    private String role;
 
     public Usuario() {
     }
 
-    public Usuario(String uuid, String userName, String password, String role) {
+    public Usuario(String uuid, String userName, String password) {
         this.uuid = uuid;
         this.userName = userName;
         this.password = password;
-        this.role = role;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 }
