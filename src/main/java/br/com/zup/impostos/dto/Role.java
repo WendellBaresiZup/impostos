cfd01cc39dfa2d11c4c8a1ce5553cf77a93b4ed7
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-10 15:13:55 -0300

    feat: Cria a enum Role com role_admin e role_user

--- src/main/java/br/com/zup/impostos/dto/Role.java
+++ src/main/java/br/com/zup/impostos/dto/Role.java
@@ -1,4 +1,16 @@
 package br.com.zup.impostos.dto;
 
 public enum Role {
+    ROLE_ADMIN("ADMIN"),
+    ROLE_USER("USER");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName(){
+        return this.name();
+    }
 }
