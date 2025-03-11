d0e4a93cd64738d0c5ec318fbfaa192a4429b13a
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 07:57:17 -0300

    refactor: Altera metodos cadastrarUsuario e Login implementando parametros de UsuarioDTO, UsuarioLoginDTO e UsuarioResponseDTO

--- src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
@@ -2,44 +2,49 @@ package br.com.zup.impostos.services;
 
 import br.com.zup.impostos.dto.UsuarioDTO;
 import br.com.zup.impostos.dto.UsuarioLoginDTO;
+import br.com.zup.impostos.exceptions.UsuarioInvalidException;
+import br.com.zup.impostos.exceptions.UsuarioNotFoundException;
 import br.com.zup.impostos.infra.JwtUtil;
 import br.com.zup.impostos.models.Usuario;
 import br.com.zup.impostos.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
 
-    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-    private final JwtUtil jwtUtil;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, JwtUtil jwtUtil) {
-        this.usuarioRepository = usuarioRepository;
-        this.jwtUtil = jwtUtil;
-    }
+    private Map<String, String> roleCache = new HashMap<>();
 
 
     @Override
     public Usuario cadastrarUsuario(UsuarioDTO usuarioDTO){
         if (usuarioRepository.findByUserName(usuarioDTO.getUserName()).isPresent()){
-            throw new RuntimeException("Username já existe!");
+            throw new UsuarioInvalidException("Username já existe!");
         }
         Usuario usuario = new Usuario();
         usuario.setUserName(usuarioDTO.getUserName());
         usuario.setPassword(new BCryptPasswordEncoder().encode(usuarioDTO.getPassword()));
-        usuario.setRole(usuarioDTO.getRole());
         return usuarioRepository.save(usuario);
     }
 
     @Override
     public String login(UsuarioLoginDTO usuarioLoginDTO){
         Usuario usuario = (Usuario) usuarioRepository.findByUserName(usuarioLoginDTO.getUserName())
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!") );
+                .orElseThrow(() -> new UsuarioNotFoundException("Usuário não encontrado!") );
         if (!new BCryptPasswordEncoder().matches(usuarioLoginDTO.getPassword(), usuario.getPassword())){
-            throw new RuntimeException("Senha inválida!");
+            throw new UsuarioInvalidException("Senha inválida!");
         }
-        return jwtUtil.geradorToken(usuario.getUserName());
+        String role = roleCache.get(usuarioLoginDTO.getUserName());
+        List<String> roles = Arrays.asList(role);
+        return jwtUtil.geradorToken(usuario.getUserName(), roles);
     }
 }
