ac407aa8baa98050021873c94443c2eea30eb8ef
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 08:10:21 -0300

    feat: Cria classe UsuarioResponseDTO com atributos e metodos com anotation @Data

--- src/main/java/br/com/zup/impostos/dto/UsuarioResponseDTO.java
+++ src/main/java/br/com/zup/impostos/dto/UsuarioResponseDTO.java
@@ -1,4 +1,19 @@
 package br.com.zup.impostos.dto;
 
+import lombok.Data;
+
+@Data
 public class UsuarioResponseDTO {
+    private Long id;
+    private String userName;
+    private Role role;
+
+    public UsuarioResponseDTO(){
+    }
+
+    public UsuarioResponseDTO(Long id, String userName, Role role) {
+        this.id = id;
+        this.userName = userName;
+        this.role = role;
+    }
 }
