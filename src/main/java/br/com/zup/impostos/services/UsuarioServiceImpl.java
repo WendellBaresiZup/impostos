0c6a56f91be5540e9f7f9817a79a12ff3e079837
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 15:04:02 -0300

    refactor: Altera metodo cadastrarUsuario para passar o parametro da Role

--- src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/zup/impostos/services/UsuarioServiceImpl.java
@@ -22,18 +22,21 @@ public class UsuarioServiceImpl implements UsuarioService{
     @Autowired
     private JwtUtil jwtUtil;
 
-    private Map<String, String> roleCache = new HashMap<>();
-
-
     @Override
     public Usuario cadastrarUsuario(UsuarioDTO usuarioDTO){
-        if (usuarioRepository.findByUserName(usuarioDTO.getUserName()).isPresent()){
-            throw new UsuarioInvalidException("Username já existe!");
+        try {
+            if (usuarioRepository.findByUserName(usuarioDTO.getUserName()).isPresent()){
+                throw new UsuarioInvalidException("Username já existe!");
+            }
+            Usuario usuario = new Usuario();
+            usuario.setUserName(usuarioDTO.getUserName());
+            usuario.setPassword(new BCryptPasswordEncoder().encode(usuarioDTO.getPassword()));
+            usuario.setRole(usuarioDTO.getRole());
+            return usuarioRepository.save(usuario);
+        } catch (UsuarioInvalidException e){
+            System.out.println("Erro ao cadastrar usuario: " + e.getMessage());
+            return null;
         }
-        Usuario usuario = new Usuario();
-        usuario.setUserName(usuarioDTO.getUserName());
-        usuario.setPassword(new BCryptPasswordEncoder().encode(usuarioDTO.getPassword()));
-        return usuarioRepository.save(usuario);
     }
 
     @Override
@@ -43,8 +46,8 @@ public class UsuarioServiceImpl implements UsuarioService{
         if (!new BCryptPasswordEncoder().matches(usuarioLoginDTO.getPassword(), usuario.getPassword())){
             throw new UsuarioInvalidException("Senha inválida!");
         }
-        String role = roleCache.get(usuarioLoginDTO.getUserName());
-        List<String> roles = Arrays.asList(role);
+        List<String> roles = Collections.singletonList(usuario.getRole().name());
+        System.out.println("Role do usuário " + usuario.getUserName() + ": " + roles);
         return jwtUtil.geradorToken(usuario.getUserName(), roles);
     }
 }
