ea673e14d686e869df127563abd8f73e8cc11632
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 08:08:00 -0300

    refactor: Implementa anotation @Data

--- src/main/java/br/com/zup/impostos/dto/ImpostoDTO.java
+++ src/main/java/br/com/zup/impostos/dto/ImpostoDTO.java
@@ -1,4 +1,18 @@
 package br.com.zup.impostos.dto;
 
+import lombok.Data;
+
+@Data
 public class ImpostoDTO {
+    private String nome;
+    private String descricao;
+    private double aliquota;
+
+    public ImpostoDTO(){}
+
+    public ImpostoDTO(String nome, String descricao, double aliquota) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.aliquota = aliquota;
+    }
 }
