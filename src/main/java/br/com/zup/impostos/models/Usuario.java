08d26e1d8760311aa12d5d6448cd1122b33201ad
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 14:55:59 -0300

    refactor: Implementa a enum Role como atributo

--- src/main/java/br/com/zup/impostos/models/Usuario.java
+++ src/main/java/br/com/zup/impostos/models/Usuario.java
@@ -1,6 +1,9 @@
 package br.com.zup.impostos.models;
 
+import br.com.zup.impostos.dto.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -16,13 +19,16 @@ public class Usuario {
     private String userName;
     @NotBlank(message = "Senha é obrigatória")
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public Usuario() {
     }
 
-    public Usuario(String uuid, String userName, String password) {
+    public Usuario(String uuid, String userName, String password, Role role) {
         this.uuid = uuid;
         this.userName = userName;
         this.password = password;
+        this.role = role;
     }
 }
