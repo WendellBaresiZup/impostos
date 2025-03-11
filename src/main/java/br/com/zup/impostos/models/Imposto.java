93979874a5367dec5778be2a43c13f39a22b5993
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-11 07:55:37 -0300

    refactor: Implementa anotation @Data

--- src/main/java/br/com/zup/impostos/models/Imposto.java
+++ src/main/java/br/com/zup/impostos/models/Imposto.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
-@Entity
+@Data
+@Entity(name = "IMPOSTO")
 public class Imposto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,7 +15,6 @@ public class Imposto {
     private String nome;
     private String descricao;
     private double aliquota;
-
 
 
     public Imposto(Long id, String nome, String descricao, double aliquota) {
@@ -24,37 +25,5 @@ public class Imposto {
     }
 
     public Imposto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public double getAliquota() {
-        return aliquota;
-    }
-
-    public void setAliquota(double aliquota) {
-        this.aliquota = aliquota;
     }
 }
