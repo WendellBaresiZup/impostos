730512700ffa23461730a9a81420bef73bcca933
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-12 14:46:23 -0300

    refactor: Exclui metodo de Decoder

--- src/main/java/br/com/zup/impostos/infra/SecurityConfig.java
+++ src/main/java/br/com/zup/impostos/infra/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.zup.impostos.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,25 +10,31 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(request ->request
                 .requestMatchers("/api/usuario/cadastrar", "/api/usuario/login").permitAll()
-                .requestMatchers(HttpMethod.POST,"/api/tipos", "/api/tipos/calculo").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/api/tipos", "/api/tipos/calculo").hasAnyAuthority("ROLE_ADMIN")
                 .requestMatchers(HttpMethod.GET,"/api/tipos").authenticated()
                 .anyRequest().authenticated()
         );
         http.csrf(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-        @Bean
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
