11aca2353764c7c68c77d5f007d07f6e8d82f4a1
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-13 14:53:25 -0300

    feat: Cria classe CalculoImpostoResponse com atributos e metodos

--- src/main/java/br/com/zup/impostos/dto/CalculoImpostoResponse.java
+++ src/main/java/br/com/zup/impostos/dto/CalculoImpostoResponse.java
@@ -1,4 +1,22 @@
 package br.com.zup.impostos.dto;
 
+import lombok.Data;
+
+@Data
 public class CalculoImpostoResponse {
+    private String tipoImposto;
+    private double valorBase;
+    private double aliquota;
+    private double valorImposto;
+
+    public CalculoImpostoResponse(String tipoImposto, double valorBase, double aliquota, double valorImposto) {
+        this.tipoImposto = tipoImposto;
+        this.valorBase = valorBase;
+        this.aliquota = aliquota;
+        this.valorImposto = valorImposto;
+    }
+
+    public CalculoImpostoResponse(){
+
+    }
 }
