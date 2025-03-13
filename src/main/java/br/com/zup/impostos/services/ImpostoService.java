4e6e79cf1931d2967a7693ccfdf4df8d10a4d12c
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-13 14:54:54 -0300

    feat: Cria metodo de calcularImpostoResponse. refacto: Altera parametros do metodo calcularImposto

--- src/main/java/br/com/zup/impostos/services/ImpostoService.java
+++ src/main/java/br/com/zup/impostos/services/ImpostoService.java
@@ -1,15 +1,16 @@
 package br.com.zup.impostos.services;
 
+import br.com.zup.impostos.dto.CalculoImpostoResponse;
 import br.com.zup.impostos.dto.ImpostoDTO;
 import br.com.zup.impostos.models.Imposto;
-import br.com.zup.impostos.repositories.ImpostoRepository;
 
 import java.util.List;
 
 public interface ImpostoService {
-    Double calcularImposto(Imposto imposto, double valorBase);
+    Double calcularImposto(Long impostoId, double valorBase);
     List<Imposto> listarTodosImpostos();
     Imposto listarImpostoPeloId(Long id);
     Imposto cadastrarImposto(ImpostoDTO impostoDTO);
     void deletarImpostoPeloId(Long id);
+    CalculoImpostoResponse calcularImpostoResponse(Long impostoId, double valorBase);
 }
