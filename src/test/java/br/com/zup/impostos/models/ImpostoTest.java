249099a4651f4605f2078abc737180baf163348b
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-17 15:55:16 -0300

    test: Cria classe UsuarioTest. Cria metodo UsuarioTeste

--- src/test/java/br/com/zup/impostos/models/ImpostoTest.java
+++ src/test/java/br/com/zup/impostos/models/ImpostoTest.java
@@ -1,4 +1,19 @@
 package br.com.zup.impostos.models;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ImpostoTest {
+
+    @Test
+    public void impostoTeste(){
+        Imposto imposto = new Imposto(1L, "IR", "Imposto sobre a renda de pessoas físicas",27.5);
+
+        assertEquals(1L,imposto.getId());
+        assertEquals("IR", imposto.getNome());
+        assertEquals("Imposto sobre a renda de pessoas físicas", imposto.getDescricao());
+        assertEquals(27.5, imposto.getAliquota());
+    }
+
 }
