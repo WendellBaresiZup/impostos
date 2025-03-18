7d5f4a72d47089ae37ae98aed0ab3cf3cb1f9808
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 14:52:26 -0300

    test: Cria classe UsuarioDTOTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/dto/UsuarioDTOTest.java
+++ src/test/java/br/com/zup/impostos/dto/UsuarioDTOTest.java
@@ -1,4 +1,34 @@
 package br.com.zup.impostos.dto;
 
+import lombok.Data;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class UsuarioDTOTest {
+
+    private String userName;
+    private String password;
+    private Role role;
+    private UsuarioDTO usuarioDTO;
+
+    @BeforeEach
+    public void setUp(){
+        userName = "TesteUsuario";
+        password = "password";
+        role = Role.ROLE_USER;
+
+        usuarioDTO = new UsuarioDTO(userName,password, role);
+    }
+
+    @Test
+    public void usuarioDTOTeste(){
+        assertEquals(userName, usuarioDTO.getUserName());
+        assertEquals(password, usuarioDTO.getPassword());
+        assertEquals(role, usuarioDTO.getRole());
+    }
 }
