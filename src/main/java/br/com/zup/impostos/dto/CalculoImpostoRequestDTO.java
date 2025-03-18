57126e99d30124ecedf1ac18fc29d03d53f8f956
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 10:07:17 -0300

    refactor: Altera nome da classe

--- src/main/java/br/com/zup/impostos/dto/CalculoImpostoRequestDTO.java
+++ src/main/java/br/com/zup/impostos/dto/CalculoImpostoRequestDTO.java
@@ -3,15 +3,15 @@ package br.com.zup.impostos.dto;
 import lombok.Data;
 
 @Data
-public class CalculoImpostoRequest {
+public class CalculoImpostoRequestDTO {
     private Long tipoImpostoId;
     private double valorBase;
 
-    public CalculoImpostoRequest(Long tipoImpostoId, double valorBase) {
+    public CalculoImpostoRequestDTO(Long tipoImpostoId, double valorBase) {
         this.tipoImpostoId = tipoImpostoId;
         this.valorBase = valorBase;
     }
 
-    public CalculoImpostoRequest() {
+    public CalculoImpostoRequestDTO() {
     }
 }
