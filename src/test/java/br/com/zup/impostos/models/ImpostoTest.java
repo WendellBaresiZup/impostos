c6e022ca81c21c981ac1fccd62e321f4f47ab07a
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 09:56:14 -0300

    test: Passa anotations do mockito na classe

--- src/test/java/br/com/zup/impostos/models/ImpostoTest.java
+++ src/test/java/br/com/zup/impostos/models/ImpostoTest.java
@@ -1,9 +1,12 @@
 package br.com.zup.impostos.models;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class ImpostoTest {
 
     @Test
