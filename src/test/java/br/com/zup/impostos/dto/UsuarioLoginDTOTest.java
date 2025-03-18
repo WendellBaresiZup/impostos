b9b82ead4d54e536f5515a338c32e8c74716ed41
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 14:52:54 -0300

    test: Cria classe UsuarioLoginDTOTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/dto/UsuarioLoginDTOTest.java
+++ src/test/java/br/com/zup/impostos/dto/UsuarioLoginDTOTest.java
@@ -1,4 +1,46 @@
 package br.com.zup.impostos.dto;
 
+import lombok.Data;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UsuarioLoginDTOTest {
+
+    private String userName;
+    private String password;
+    private UsuarioLoginDTO usuarioLoginDTO;
+
+    @BeforeEach
+    public void setUp(){
+        userName = "TesteUsuario";
+        password = "password";
+
+        usuarioLoginDTO = new UsuarioLoginDTO(userName,password);
+    }
+
+    @Test
+    public void usarioLoginDTOTeste(){
+        assertEquals("TesteUsuario", usuarioLoginDTO.getUserName());
+        assertEquals("password", usuarioLoginDTO.getPassword());
+    }
+
+
+
+
+    @Data
+    public class UsuarioLoginDTO {
+        private String userName;
+        private String password;
+
+        public UsuarioLoginDTO(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public UsuarioLoginDTO(){
+        }
+
+    }
 }
