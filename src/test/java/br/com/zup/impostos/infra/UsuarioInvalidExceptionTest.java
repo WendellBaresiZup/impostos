0ccaa1136cbf5ad49956222a5fdfadcf86dee773
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 10:05:12 -0300

    test: Cria classe UsuarioInvalidExceptionTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/infra/UsuarioInvalidExceptionTest.java
+++ src/test/java/br/com/zup/impostos/infra/UsuarioInvalidExceptionTest.java
@@ -1,4 +1,27 @@
 package br.com.zup.impostos.infra;
 
+import br.com.zup.impostos.exceptions.UsuarioInvalidException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class UsuarioInvalidExceptionTest {
+    private String message;
+    private UsuarioInvalidException exception;
+
+    @BeforeEach
+    public void setUp(){
+        message = "Usuario invalido";
+
+        exception = new UsuarioInvalidException(message);
+    }
+
+    @Test
+    public void usuarioInvalidExceptionTeste(){
+        assertEquals(message, exception.getMessage());
+    }
 }
