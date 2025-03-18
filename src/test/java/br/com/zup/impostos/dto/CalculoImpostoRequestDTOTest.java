62c5d66bd33da75ff3db4ce3d49b11e30257c735
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 10:14:50 -0300

    test: Cria classe CalculoImpostoRequestDTOTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/dto/CalculoImpostoRequestDTOTest.java
+++ src/test/java/br/com/zup/impostos/dto/CalculoImpostoRequestDTOTest.java
@@ -1,10 +1,32 @@
 package br.com.zup.impostos.dto;
 
+import br.com.zup.impostos.models.Imposto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-public class CalculoImpostoRequestTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public void
+@ExtendWith(MockitoExtension.class)
+public class CalculoImpostoRequestDTOTest {
+
+    private Long tipoImpostoId;
+    private double valorBase;
+    private CalculoImpostoRequestDTO requestDTO;
+
+    @BeforeEach
+    public void setUp(){
+        tipoImpostoId = 1L;
+        valorBase = 1000.0;
+
+        requestDTO = new CalculoImpostoRequestDTO(tipoImpostoId, valorBase);
+    }
+
+
+    @Test
+    public void CalculoImpostoRequestDTOTeste(){
+        assertEquals(tipoImpostoId, requestDTO.getTipoImpostoId());
+        assertEquals(valorBase, requestDTO.getValorBase());
+    }
 }
