80d8d501be04686cfbc88a8ce0475b7f40e870d4
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 10:02:23 -0300

    test: Cria classe RoleInvalidExceptionTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/infra/RoleInvalidExceptionTest.java
+++ src/test/java/br/com/zup/impostos/infra/RoleInvalidExceptionTest.java
@@ -1,4 +1,27 @@
 package br.com.zup.impostos.infra;
 
+import br.com.zup.impostos.exceptions.RoleInvalidException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class RoleInvalidExceptionTest {
+    private String message;
+    private RoleInvalidException exception;
+
+    @BeforeEach
+    public void setUp(){
+        message = "ROLE_ADMIN E ROLE_USER";
+
+        exception = new RoleInvalidException(message);
+    }
+
+    @Test
+    public void roleInvalidExceptionTeste(){
+        assertEquals(message, exception.getMessage());
+    }
 }
