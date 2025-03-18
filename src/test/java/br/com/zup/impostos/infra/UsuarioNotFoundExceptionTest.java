08681a564e8ef18459ed57bcf15757e5aa442785
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 09:59:15 -0300

    test: Cria classe UsuarioNotFoundExceptionTest e metodo do teste para fazer os testes da classe principal

--- src/test/java/br/com/zup/impostos/infra/UsuarioNotFoundExceptionTest.java
+++ src/test/java/br/com/zup/impostos/infra/UsuarioNotFoundExceptionTest.java
@@ -1,4 +1,28 @@
 package br.com.zup.impostos.infra;
 
+import br.com.zup.impostos.exceptions.UsuarioNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class UsuarioNotFoundExceptionTest {
+
+    private String message;
+    private UsuarioNotFoundException exception;
+
+    @BeforeEach
+    public void setUp(){
+        message = "Usuario nao encontrado";
+
+        exception = new UsuarioNotFoundException(message);
+    }
+
+    @Test
+    public void usuarioNotFoundExceptionTeste(){
+        assertEquals(message, exception.getMessage());
+    }
 }
