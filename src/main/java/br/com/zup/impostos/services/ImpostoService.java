9fea02ba3a1e736d8335f42533804fba09f3ec78
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 10:10:31 -0300

    refactor: Passa parametros da alteracao da classe CalculoImpostoRequestDTO e CalculoImpostoResponseDTO

--- src/main/java/br/com/zup/impostos/services/ImpostoService.java
+++ src/main/java/br/com/zup/impostos/services/ImpostoService.java
@@ -1,6 +1,6 @@
 package br.com.zup.impostos.services;
 
-import br.com.zup.impostos.dto.CalculoImpostoResponse;
+import br.com.zup.impostos.dto.CalculoImpostoResponseDTO;
 import br.com.zup.impostos.dto.ImpostoDTO;
 import br.com.zup.impostos.models.Imposto;
 
@@ -12,5 +12,5 @@ public interface ImpostoService {
     Imposto listarImpostoPeloId(Long id);
     Imposto cadastrarImposto(ImpostoDTO impostoDTO);
     void deletarImpostoPeloId(Long id);
-    CalculoImpostoResponse calcularImpostoResponse(Long impostoId, double valorBase);
+    CalculoImpostoResponseDTO calcularImpostoResponse(Long impostoId, double valorBase);
 }
