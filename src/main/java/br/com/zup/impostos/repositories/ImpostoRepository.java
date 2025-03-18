a3bb5ab9fdbd7885b2c5c2b9cf20f55638d9cd29
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 14:55:01 -0300

    refactor: Passa anotattion para os teste ler a classe como repository

--- src/main/java/br/com/zup/impostos/repositories/ImpostoRepository.java
+++ src/main/java/br/com/zup/impostos/repositories/ImpostoRepository.java
@@ -2,8 +2,8 @@ package br.com.zup.impostos.repositories;
 
 import br.com.zup.impostos.models.Imposto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface ImpostoRepository extends JpaRepository<Imposto, Long> {
 }
