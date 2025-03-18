21297de892cfede395e70a90d3c38330daaa0ea0
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-18 16:55:39 -0300

    feat: Cria classe GlobalExceptionHandler para trabalhar as excecoes globais

---
+++ src/main/java/br/com/zup/impostos/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package br.com.zup.impostos.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException{
+
+        @ExceptionHandler(RuntimeException.class)
+        public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+        }
+}
+
