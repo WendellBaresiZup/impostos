32e1f9fe77e698ab9146144d11d64f3f41054a6f
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-03-19 12:27:22 -0300

    test: Cria classe JwtAuthenticationFilterTest com teste dos metodos da classe principal

--- src/test/java/br/com/zup/impostos/infra/JwtAuthenticationFilterTest.java
+++ src/test/java/br/com/zup/impostos/infra/JwtAuthenticationFilterTest.java
@@ -1,4 +1,104 @@
 package br.com.zup.impostos.infra;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationFilterTest {
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private UserDetailsService userDetailsService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @InjectMocks
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    private UserDetails userDetails;
+
+    @BeforeEach
+    public void setUp(){
+        userDetails = new User("TesteUsuario", "TestePassword", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    public void doFilterInternalValidTeste() throws ServletException, IOException {
+        String token = "Token";
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+        when(jwtUtil.validateToken(token)).thenReturn(true);
+        when(jwtUtil.getUserNameFromToken(token)).thenReturn("TesteUsuario");
+        when(userDetailsService.loadUserByUsername("TesteUsuario")).thenReturn(userDetails);
+
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+        assertEquals("TesteUsuario", SecurityContextHolder.getContext().getAuthentication().getName());
+        verify(filterChain).doFilter(request,response);
+    }
+
+    @Test
+    public void doFilterInternalInvalidTeste() throws ServletException, IOException{
+        String token = "invalidToken";
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+        when(jwtUtil.validateToken(token)).thenReturn(false);
+
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(filterChain).doFilter(request,response);
+    }
+
+    @Test
+    public void doFilterInternalNoTokenTeste() throws ServletException, IOException{
+        when(request.getHeader("Authorization")).thenReturn(null);
+
+        jwtAuthenticationFilter.doFilterInternal(request,response,filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(filterChain).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilterInternalNoBearerTeste() throws ServletException, IOException{
+        String token = "Token";
+        when(request.getHeader("Authorization")).thenReturn(token);
+
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(filterChain).doFilter(request, response);
+    }
 }
